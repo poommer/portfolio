770c1a051d7de3b4dff043997375e9951b49b466
Siripoom Limsoontorn <110332113+poommer@users.noreply.github.com>
2023-12-11 17:27:18 +0700

    finish

--- js/script.js
+++ js/script.js
@@ -10,4 +10,73 @@ $(document).ready(function(){
         $('#Btn_menu').show()
         $('#Btn_menuClose').hide()
     })
+
+
+    $('#BtnTobe').click(function(){
+        $('.ToBeIT').show()
+        
+    })
+
+    $('#BtnComSkill').click(function(){
+        $('.ComSkill').show()
+    })
+
+    $('#BtnAI').click(function(){
+        $('.AI').show()
+    })
+
+$('#BtnData').click(function(){
+        $('.Data').show()
+    })
+
+$('#BtnMeetingIBMer').click(function(){
+        $('.MeetingIBMer').show()
+    })
+
+    $('#BtnIBM').click(function(){
+        $('.IBM').show()
+    })
+
+
+
+    $('.pop-close').click(function(){
+         $('.ToBeIT').hide()
+       $('.ComSkill').hide()
+         $('.AI').hide()
+         $('.Data').hide()
+         $('.MeetingIBMer').hide()
+         $('.IBM').hide()
+    })
+
+
+
+
+    $('#prev').click(function(){
+        
+    })
 })
+
+
+// -------------------
+
+var slideIndex = 1;
+ShowImg(slideIndex);
+
+function plusSlides(n) {
+    ShowImg(slideIndex += n);
+}
+
+// function currentSlide(n) {
+//     ShowImg(slideIndex = n);
+// }
+
+function ShowImg(n) {
+    var i;
+    var slides = document.getElementsByClassName("Act-img");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
+}
